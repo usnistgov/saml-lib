9ae360adc3c3c2111271b97caaaa773bc20eeff1
sriniadhiwork <sriniadhi.work@gmail.com>
2016-11-16 16:09:02 -0500

    Update OpenSamlWsseSecurityGenerator.java

--- src/main/java/gov/nist/hit/ds/wsseTool/generation/opensaml/OpenSamlWsseSecurityGenerator.java
+++ src/main/java/gov/nist/hit/ds/wsseTool/generation/opensaml/OpenSamlWsseSecurityGenerator.java
@@ -48,6 +48,24 @@ public class OpenSamlWsseSecurityGenerator {
 	private static NamespaceContext namespaces = NhwinNamespaceContextFactory.getDefaultContext();
 
 	private Context context;
+	
+	public static Document rdoc(Document doc) throws XPathExpressionException {
+		XPathFactory xpathFactory = XPathFactory.newInstance();
+		// XPath to find empty text nodes.
+		XPathExpression xpathExp = xpathFactory.newXPath().compile(
+		        "//text()[normalize-space(.) = '']");  
+		NodeList emptyTextNodes = (NodeList) 
+		        xpathExp.evaluate(doc, XPathConstants.NODESET);
+
+		// Remove each empty text node from document.
+		for (int i = 0; i < emptyTextNodes.getLength(); i++) {
+		    Node emptyTextNode = emptyTextNodes.item(i);
+		    if (emptyTextNode != null)
+		    	emptyTextNode.getParentNode().removeChild(emptyTextNode);
+		}
+		
+		return doc;
+	}
 
 	public Document generateWsseHeader(GenContext context) throws GenerationException {
 
@@ -82,7 +100,7 @@ public class OpenSamlWsseSecurityGenerator {
 
 			log.info("marshall back to DOM");
 			securityHeader = saml.marshallAsNewDOMDocument(sec.getSecurity());
-
+			rdoc(securityHeader);
 			log.info("update message template");
 			updateTemplate(securityHeader);
 
